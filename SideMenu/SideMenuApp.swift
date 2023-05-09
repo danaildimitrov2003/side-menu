ccd4a691fe5b23f26f70c4f990d8aacd2f33460c
dvny <danaildimitrov2003@gmail.com>
2023-05-09 11:59:24 +0300

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Danail Dimitrov on 9.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
